461059a7c03c974e140990135ea93dca95ff003e
Yubor Molina <yubor98@gmail.com>
2022-05-12 23:05:33 -0500

    Correcting routing

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AllCharacters/>} />
-          <Route path='/character/:id' element={<OneCharacter/>} />
+          <Route path="/rick-and-morty" element={<AllCharacters/>} />
+          <Route path='/rick-and-morty/character/:id' element={<OneCharacter/>} />
         </Routes>
       </BrowserRouter>
     </div>
